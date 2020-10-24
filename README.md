308dba61e621ba8f66efbaff565e09576f71fd0f
Lars Jacobsson <lars.jacobsson@mathem.se>
2020-10-25 00:37:21 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# cfn-diagram-ci
+
+Extension of [cfn-diagram](https://github.com/mhlabs/cfn-diagram) that lets you take static screenshots of generated diagrams. 
+
+Still experimental and work in progress
---
+++ index.js
@@ -0,0 +1,14 @@
+const Pageres = require("pageres");
+process.argv.push("--ci-mode");
+require("@mhlabs/cfn-diagram/index");
+const tempDir = require("temp-dir");
+const path = require("path");
+
+if (process.argv.includes("html")) {
+  new Pageres({ filename: "cfn-diagram" })
+    .src(`file://${path.join(tempDir, "cfn-diagram", "index.html")}`, ["1024x768"], {
+      crop: true,
+    })
+    .dest(".")
+    .run();
+}
